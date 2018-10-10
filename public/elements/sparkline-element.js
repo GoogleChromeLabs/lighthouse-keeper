6c6f34d0f27a0ed9c7bfad51cd479298afd68c97
Eric Bidelman <ebidel@gmail.com>
2018-10-10 16:17:12 -0700

    Copyrights

--- public/elements/sparkline-element.js
+++ public/elements/sparkline-element.js
@@ -1,10 +1,20 @@
-//const DevsiteCustomElement = goog.require('devsite.app.CustomElement');
-//const lit = goog.require('lit-html');
-//const {html, render} = lit;
+/**
+ * Copyright 2018 Google Inc., PhantomJS Authors All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-// import {html, render} from '../../node_modules/lit-html/lit-html.js';
 import {html, render} from '../node_modules/lit-html/lit-html.js';
-// import {repeat} from './lit-html/lib/repeat.js';
 
 const clampTo2Decimals = val => Math.round(val * 100) / 100;
 
