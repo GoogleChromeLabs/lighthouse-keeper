7f4b8aa34cdc4fd74392ad45ef0af21f90fef63b
Eric Bidelman <ebidel@gmail.com>
2018-10-18 14:29:25 -0700

    Update license headers

--- memcache.mjs
+++ memcache.mjs
@@ -1,5 +1,5 @@
 /**
- * Copyright 2018 Google Inc., PhantomJS Authors All rights reserved.
+ * Copyright 2018 Google Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
