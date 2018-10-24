1b2d09992585d898f1f626e7ccfe8b93f3d0b6ef
Eric Bidelman <ebidel@gmail.com>
2018-10-24 09:20:50 -0700

    Update lighthouse-data.mjs

--- lighthouse-data.mjs
+++ lighthouse-data.mjs
@@ -331,7 +331,7 @@ export async function getReports(url,
 
 /**
  * Deletes a subcollection in Firestore by batch.
- * @param {Object} query Firestore subcollection query.
+ * @param {!Object} query Firestore subcollection query.
  * @param {Function} resolve Function to call when all batches are deleted.
  * @param {Function} reject Function to call in case of error.
  */
